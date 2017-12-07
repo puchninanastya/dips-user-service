72ba9bceb52d1c22bad78d4188c3c46f6a2fd40d
Анастасия Пучнина <puchnina.nastya@gmail.com>
2017-12-07 23:49:59 +0300

    Profile migration, Create Profile test added.

--- user_service/models.py
+++ user_service/models.py
@@ -3,4 +3,6 @@ from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    pass
+    user = models.OneToOneField(User)
+    phone_regex = RegexValidator(regex=r'^\++[7]+\d{10}$', message="Phone number must be entered in the format: '+79999999999'.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=12, blank=True)
--- user_service/tests/test_models.py
+++ user_service/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile
+from user_service.models import Profile
 
 class ModelTestCase(TestCase):
     """This class defines the test suite for the Profile model."""
@@ -8,31 +8,31 @@ class ModelTestCase(TestCase):
     def setUp(self):
         newUser1 = User.objects.create_user(username='john007',
             first_name='John',
-            second_name='Lennon',
+            last_name='Lennon',
             email='jlennon@beatles.com',
             password='beatlesrulit')
-        profile1 = Profile.objects.create(user=newUser1, phone_number='+79161002030')
+        self.profile1 = Profile(user=newUser1, phone_number='+79161002030')
         newUser2 = User.objects.create_user(username='vasss',
             first_name='Vasya',
-            second_name='Pupkin',
+            last_name='Pupkin',
             email='vasss@gmail.com',
             password='qwerty123')
-        profile2 = Profile.objects.create(user=newUser2, phone_number='+79094005060')
-        newUser3 = User.objects.create_user(username='vasss',
-            first_name='Vasya',
-            second_name='Pupkin',
-            email='vasss@gmail.com',
-            password='qwerty123')
-        profile3 = Profile.objects.create(user=newUser3, phone_number='+79094005060')
+        self.profile2 = Profile.objects.create(user=newUser2, phone_number='+79094005060')
+        newUser3 = User.objects.create_user(username='omega',
+            first_name='Olga',
+            last_name='Silomonova',
+            email='omega@yahoo.com',
+            password='asdfg9090')
+        self.profile3 = Profile.objects.create(user=newUser3, phone_number='+79094005060')
         newUser4 = User.objects.create_user(username='vano',
             first_name='Ivan',
-            second_name='Ivanov',
+            last_name='Ivanov',
             email='vano@yandex.ru',
             password='etomoiparol')
-        profile4 = Profile.objects.create(user=newUser4, phone_number='+79151005060')
+        self.profile4 = Profile.objects.create(user=newUser4, phone_number='+79151005060')
 
     def test_model_can_create_profile(self):
-        """Test the bucketlist model can create a Profile."""
+        """Test the profile model can create a Profile."""
         old_count = Profile.objects.count()
         self.profile1.save()
         new_count = Profile.objects.count()
