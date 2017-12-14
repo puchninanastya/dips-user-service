c8f449a00aa2e4bad7594a9d0e236a6cf7767d39
Анастасия Пучнина <puchnina.nastya@gmail.com>
2017-12-14 22:01:04 +0300

    Views create and delete mixins added. Import fixed

--- user_service/views.py
+++ user_service/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets, mixins
-from rest_framework.views import APIView
-from rest_framework.request import Request
-from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import UserSerializer, ProfileSerializer
@@ -12,12 +8,12 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 #TODO: Create tests for views
-#TODO: Create pagination
-#TODO: Return to super views?
 
 class UserViewSet(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
                      mixins.RetrieveModelMixin,
                      mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
                      viewsets.GenericViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
